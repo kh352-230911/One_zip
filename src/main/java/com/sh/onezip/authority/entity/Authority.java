0a0ff15d8d0b91ee27f36961efe8c0b989d91d11
NLGZ <edas96@naver.com>
2024-02-07 14:05:44 +0900

    nono

--- src/main/java/com/sh/onezip/authority/entity/Authority.java
+++ src/main/java/com/sh/onezip/authority/entity/Authority.java
@@ -1,6 +1,7 @@
 package com.sh.onezip.authority.entity;
 
 import com.sh.onezip.authority.entity.RoleAuth;
+import com.sh.onezip.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,12 +21,20 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Authority {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "seq_Authority_id_generator")
+    @SequenceGenerator(
+            name = "seq_Authority_id_generator",
+            sequenceName = "tb_authority_seq",
+            initialValue = 1,
+            allocationSize = 1)
     private Long id;
     @Column(nullable = false, name = "member_id")
     private String memberId;
+
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private RoleAuth userType;
+
 
 }
