b1f48e8e362facce268249eea9a7e7b8792043e9
jinwoo2361 <jinwoo236161@gmail.com>
2024-02-13 14:47:45 +0900

    Feat: diary list,detail

---
+++ src/main/java/com/sh/onezip/tip/repository/TipRepository.java
@@ -0,0 +1,8 @@
+package com.sh.onezip.tip.repository;
+
+import com.sh.onezip.tip.entity.Tip;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipRepository extends JpaRepository<Tip, Long> {
+
+}
