13244dba8dcf48583c725b0bd3bc75dae3df3fa4
jinwoo2361 <jinwoo236161@gmail.com>
2024-02-13 10:46:44 +0900

    Feat: 충돌 해결

--- src/main/java/com/sh/onezip/config/S3Config.java
+++ src/main/java/com/sh/onezip/config/S3Config.java
@@ -9,29 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-<<<<<<< HEAD
-public class S3Config{
 
-//    @Value("${cloud.aws.credentials.access-key}")
-//    private String accessKey;
-//    @Value("${cloud.aws.credentials.secret-key}")
-//    private String secretKey;
-//    @Value("${cloud.aws.region.static}")
-//    private String region;
-//
-//    @Bean
-//    public AmazonS3Client amazonS3Client() {
-//        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-//        AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(credentials);
-//        return (AmazonS3Client) AmazonS3ClientBuilder
-//                .standard()
-//                .withCredentials(awsStaticCredentialsProvider)
-//                .withRegion(region)
-//                .build();
-//    }
-}
-
-=======
 public class S3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
@@ -52,4 +30,4 @@ public class S3Config {
                                     .build();
     }
 }
->>>>>>> 970b6c5 (Feat: attachment 수정)
+
