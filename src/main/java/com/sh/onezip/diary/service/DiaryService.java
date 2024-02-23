6152fcd3179ecdb04d704aac50f0ea3839ccfa8b
MyeongJun0911 <144641842+MyeongJun0911@users.noreply.github.com>
2024-02-23 16:04:54 +0900

    Update DiaryService.java

--- src/main/java/com/sh/onezip/diary/service/DiaryService.java
+++ src/main/java/com/sh/onezip/diary/service/DiaryService.java
@@ -9,6 +9,8 @@ import com.sh.onezip.zip.entity.Zip;
 import com.sh.onezip.zip.repository.ZipRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +25,10 @@ public class DiaryService {
     @Autowired
     private ZipRepository zipRepository;
 
+    public Page<DiaryListDto> findAll(Pageable pageable) {
+        Page<Diary> diaryPage = diaryRepository.findAll(pageable);
+        return diaryPage.map((diary) -> convertToDiaryListDto(diary)); // Page<BoardListDto>
+    }
     private DiaryListDto convertToDiaryListDto(Diary diary) {
         DiaryListDto diaryListDto = modelMapper.map(diary, DiaryListDto.class);
         diaryListDto.setZipId(
@@ -33,11 +39,11 @@ public class DiaryService {
         return diaryListDto;
     }
     public void createDiary(DiaryCreateDto diaryCreateDto, Member  member) {
-//        Zip zip = zipRepository.findByMemberId(member)
-//                .orElseThrow(() -> new RuntimeException("Zip not found for the member"));
-//
-//        // 찾은 Zip의 ID를 DiaryCreateDto에 설정합니다.
-//        diaryCreateDto.setZipId(zip.getId());
+        Zip zip = zipRepository.findByMemberId(member.getMemberId())
+                .orElseThrow(() -> new RuntimeException("Zip not found for the member"));
+
+        // 찾은 Zip의 ID를 DiaryCreateDto에 설정합니다.
+        diaryCreateDto.setZipId(zip.getId());
 
         // 나머지 다이어리 생성 로직...
         diaryRepository.save(convertToDiary(diaryCreateDto));
