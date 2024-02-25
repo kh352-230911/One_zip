0f1493c6649218d1d8281afbb8606c88bb65b8a9
bokyunghan <jsbkbr98@naver.com>
2024-02-26 01:33:06 +0900

    Feat: 사업자 로그인 중..

--- src/main/java/com/sh/onezip/config/WebSecurityConfig.java
+++ src/main/java/com/sh/onezip/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sh.onezip.config;
 
 import com.sh.onezip.auth.handler.CustomSuccessHandler;
+import com.sh.onezip.bizauthority.handler.BizCustomSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +28,7 @@ public class WebSecurityConfig {
         httpSecurity.authorizeHttpRequests((authorizeRequest -> {
             authorizeRequest
                     .requestMatchers("/", "/templates/index.html").permitAll()
-                    .requestMatchers("/auth/**","/admin/memberList.do", "/member/selectMemberType.do").permitAll()
+                    .requestMatchers("/auth/**","/business/**","/admin/memberList.do", "/member/selectMemberType.do").permitAll()
                     .requestMatchers("/member/createMember.do","/member/checkIdDuplicate.do").anonymous()
                     .requestMatchers("/business/createbusiness.do","/business/checkIdDuplicate.do").anonymous()
                     .requestMatchers("/product/**").authenticated()
@@ -57,7 +58,18 @@ public class WebSecurityConfig {
                         userInfoEndpointConfig.userService(oAuth2UserService);
                     });
         });
-
+//        httpSecurity.formLogin((formLoginConfigurer -> {
+//            formLoginConfigurer
+//                    .loginPage("/business/bizlogin.do") // 사업자 회원 로그인 페이지
+//                    .loginProcessingUrl("/business/bizlogin.do") // 사업자 회원 로그인 처리 URL
+//                    .successHandler(new BizCustomSuccessHandler()) // 사업자 회원 로그인 성공 시 처리
+//                    .permitAll();
+//        }));
+//        httpSecurity.logout(logoutConfigurer -> {
+//            logoutConfigurer
+//                    .logoutUrl("/business/bizlogout.do") // 사업자 회원 로그아웃 URL 설정
+//                    .logoutSuccessUrl("/");
+//        });
         return httpSecurity.build();
     }
     @Bean
